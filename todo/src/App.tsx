94d1746fc6027613a9be5edd1d5de4eec3f5f87b
Higor Cavalcanti <cavalcanti.higor1@gmail.com>
2023-09-22 17:44:42 -0300

    Implementando types

--- todo/src/App.tsx
+++ todo/src/App.tsx
@@ -1,10 +1,24 @@
+import { useState } from 'react';
 import * as C from './App.styles';
+import { Item } from './types/Item';
 
 const App = () => {
+  const [list, setlist] = useState<Item[]>([
+    { id: 1, name: 'Comprar um pão', done: false },
+    { id: 2, name: 'Comprar um escova', done: false }
+  ]);
+
   return (
       <C.Container>
         <C.Area>
           <C.Header>Lista de Tarefas</C.Header>
+
+          {/* Área de adicionar nova Tarefa */}
+
+          {list.map((item, index)=>(
+            <div>...</div>
+          ))}
+
         </C.Area>
       </C.Container>
   );
