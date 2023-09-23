4cbaf732f8c053694b67519f5c234b07dafaae09
Higor Cavalcanti <cavalcanti.higor1@gmail.com>
2023-09-23 13:52:26 -0300

    adicionando  input e + no componente

--- todo/src/components/ListItem/AddArea/index.tsx
+++ todo/src/components/ListItem/AddArea/index.tsx
@@ -3,7 +3,11 @@ import * as C from './styles';
 export const AddArea = () => {
     return (
         <C.Container>
-            ...
+            <div className="image">+</div>
+            <input
+                type="text"
+                placeholder="Adicione uma terefa"
+            />
         </C.Container>
     );
 }
